27c4b166de36c1ec4d69f414630173dbdf5920e2
anand-106 <anands2003106@gmail.com>
2025-09-26 22:20:13 +0530

    removed logs

--- backend/jwt.js
+++ backend/jwt.js
@@ -4,8 +4,6 @@ const JWT_SECRET = "jsgfhsvcjhvshcvsghj";
 function jwtVerify(req, res, next) {
   const token = req.headers.authorization;
 
-  console.log("the token ", token);
-
 
   jwt.verify(token, JWT_SECRET, (err, decoded) => {
     if (err) return res.json({ error: "invalid or expired token" });
